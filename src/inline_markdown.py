12519b85c0d6f29b1425f4e685ac0867f7860833
Daniel Ramirez <zoerunner@gmail.com>
2024-03-11 15:39:59 -0700

    Add link and image parsing and tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -21,6 +21,52 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         nodes.extend(split_nodes)
     return nodes
 
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != "text":
+            new_nodes.append(node)
+            continue
+        node_text = node.text
+        images = extract_markdown_images(node_text)
+        if len(images) == 0:
+            new_nodes.append(node)
+            continue
+        for image in images:
+            sections = node_text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid Markdown, image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], "text"))
+            new_nodes.append(TextNode(image[0], "image", image[1]))
+            node_text = sections[1]
+        if node_text != "":
+            new_nodes.append(TextNode(node_text, "text"))
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != "text":
+            new_nodes.append(node)
+            continue
+        node_text = node.text
+        links = extract_markdown_links(node_text)
+        if len(links) == 0:
+            new_nodes.append(node)
+            continue
+        for link in links:
+            sections = node_text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid Markdown, link section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], "text"))
+            new_nodes.append(TextNode(link[0], "link", link[1]))
+            node_text = sections[1]
+        if node_text != "":
+            new_nodes.append(TextNode(node_text, "text"))
+    return new_nodes
+
 def extract_markdown_images(text):
     return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
 
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from inline_markdown import split_nodes_delimiter, TextNode, extract_markdown_images, extract_markdown_links
+from inline_markdown import split_nodes_delimiter, TextNode, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
 
 class TestInlineMarkdown(unittest.TestCase):
     def test_split_nodes_bold_delimiter(self):
@@ -64,6 +64,93 @@ class TestInlineMarkdown(unittest.TestCase):
         multiple_links_text = "This is a [test](https://test.com) with a link and another [link](https://link.com)"
         expected_links = [("test", "https://test.com"), ("link", "https://link.com")]
         self.assertEqual(extract_markdown_links(multiple_links_text), expected_links)
+
+    def test_split_nodes_image(self):
+        old_nodes = [
+            TextNode("This is a ![test](test.png) with an image", "text"),
+            TextNode("This is another ![image](image.png)", "text"),
+        ]
+        expected_nodes = [
+            TextNode("This is a ", "text"),
+            TextNode("test", "image", "test.png"),
+            TextNode(" with an image", "text"),
+            TextNode("This is another ", "text"),
+            TextNode("image", "image", "image.png"),
+        ]
+        self.assertEqual(split_nodes_image(old_nodes), expected_nodes)
+
+        single_node = [
+            TextNode("This is a ![test](test.png) with an image", "text"),
+        ]
+        expected_nodes = [
+            TextNode("This is a ", "text"),
+            TextNode("test", "image", "test.png"),
+            TextNode(" with an image", "text"),
+        ]
+        self.assertEqual(split_nodes_image(single_node), expected_nodes)
+
+        multiple_images_node = [
+            TextNode("This is a ![test](test.png) with an image and another ![image](image.png)", "text"),
+        ]
+        expected_nodes = [
+            TextNode("This is a ", "text"),
+            TextNode("test", "image", "test.png"),
+            TextNode(" with an image and another ", "text"),
+            TextNode("image", "image", "image.png"),
+        ]
+        self.assertEqual(split_nodes_image(multiple_images_node), expected_nodes)
+
+        no_image_node = [
+            TextNode("This is a text without an image", "text"),
+        ]
+        expected_nodes = [
+            TextNode("This is a text without an image", "text"),
+        ]
+        self.assertEqual(split_nodes_image(no_image_node), expected_nodes)
+
+    def test_split_nodes_link(self):
+        old_nodes = [
+            TextNode("This is a [test](https://test.com) with a link", "text"),
+            TextNode("This is another [link](https://link.com)", "text"),
+        ]
+        expected_nodes = [
+            TextNode("This is a ", "text"),
+            TextNode("test", "link", "https://test.com"),
+            TextNode(" with a link", "text"),
+            TextNode("This is another ", "text"),
+            TextNode("link", "link", "https://link.com"),
+        ]
+        self.assertEqual(split_nodes_link(old_nodes), expected_nodes)
+
+        single_node = [
+            TextNode("This is a [test](https://test.com) with a link", "text"),
+        ]
+        expected_nodes = [
+            TextNode("This is a ", "text"),
+            TextNode("test", "link", "https://test.com"),
+            TextNode(" with a link", "text"),
+        ]
+        self.assertEqual(split_nodes_link(single_node), expected_nodes)
+
+        multiple_links_node = [
+            TextNode("This is a [test](https://test.com) with a link and another [link](https://link.com)", "text"),
+        ]
+        expected_nodes = [
+            TextNode("This is a ", "text"),
+            TextNode("test", "link", "https://test.com"),
+            TextNode(" with a link and another ", "text"),
+            TextNode("link", "link", "https://link.com"),
+        ]
+        self.assertEqual(split_nodes_link(multiple_links_node), expected_nodes)
+
+        no_link_node = [
+            TextNode("This is a text without a link", "text"),
+        ]
+        expected_nodes = [
+            TextNode("This is a text without a link", "text"),
+        ]
+        self.assertEqual(split_nodes_link(no_link_node), expected_nodes)
+
 
 if __name__ == "__main__":
     unittest.main()
